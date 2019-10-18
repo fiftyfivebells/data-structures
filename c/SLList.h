c3f90c9aae0e1d634ddfcf8184b9c32bde7c038b
Stephen Bell <stephen.l.bell@gmail.com>
2019-10-18 18:00:23 -0400

    Add addNode, removeHead, and removeTail signatures

--- c/SLList.h
+++ c/SLList.h
@@ -13,11 +13,14 @@ typedef struct SLList {
 /* Function Definitions */
 
 // adds given int at given index in the provided list, or to the back/fronvoid add(int i, int data, SLList *list);
+void addNode(int data, SLList *list);
 void addHead(int data, SLList *list);
 void addTail(int data, SLList *list);
 
 // removes from the given index in the provided list, frees the node, and returns the value
 int removeNode(int i, SLList *list);
+int removeHead(SLList *list);
+int removeTail(SLList *list);
 
 // get/set value at given index in the provided list
 int get(int i, SLList *list);
@@ -27,4 +30,5 @@ void set(int i, int data, SLList *list);
 SLNode* makeNode(int data);
 
 // prints out a text representation of the linked list
-void printList(SLList *node);
+void printList(SLList *list);
+
