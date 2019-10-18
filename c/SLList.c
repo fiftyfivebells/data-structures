36289a7da0a0fcb501ff90f67468c56212de58f0
Stephen Bell <stephen.l.bell@gmail.com>
2019-10-18 17:43:53 -0400

    Remove commented code

--- c/SLList.c
+++ c/SLList.c
@@ -1,17 +1,6 @@
 #include <stdlib.h>
 #include "SLList.h"
 
-/* typedef struct SLNode { */
-/*     int data; */
-/*     struct SLNode *next; */
-/* } SLNode; */
-
-/* typedef struct SLList { */
-/*     int size; */
-/*     struct SLNode *head; */
-/*     struct SLNode *tail; */
-/* } SLList; */
-
 void add(int i, int data, SLList *list) {
     int      currPos = 0;  // our current position in the list
     SLNode  *current = list->head;  // current node is the head of the list
