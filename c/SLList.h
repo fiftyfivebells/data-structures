258f8c50238c0384c0336c3a5f3966c4096b965f
Stephen Bell <stephen.l.bell@gmail.com>
2019-10-18 18:10:57 -0400

    Update function signatures for set and get

--- c/SLList.h
+++ c/SLList.h
@@ -23,8 +23,8 @@ int removeHead(SLList *list);
 int removeTail(SLList *list);
 
 // get/set value at given index in the provided list
-int get(int i, SLList *list);
-void set(int i, int data, SLList *list);
+int getNode(int i, SLList *list);
+void setNode(int i, int data, SLList *list);
 
 // creates a new node and returns a pointer to it
 SLNode* makeNode(int data);
