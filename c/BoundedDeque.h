7840e945bd3099a6d7d70441cb43b96624f44cb7
stephen <stephen.l.bell@gmail.com>
2019-10-28 08:32:32 -0400

    Add Bounded Deque header file with typedef and functions definitions

---
+++ c/BoundedDeque.h
@@ -0,0 +1,31 @@
+/* Type Definitions */
+typedef struct BoundedDeque {
+    int maxSize;    // the most elements the list can hold
+    int size;       // number of elements in list
+    int *dataList;  // list of elements
+    int front;      // index of front item in list
+} BDeque;
+
+/* Function Definitions */
+
+// Initialize the deque
+BDeque * newBoundedDeque(int maxSize);
+
+// Add Functions
+void addToIndex(int i, int data, BDeque *d);
+void addToFront(int data, BDeque *d);
+void addToBack(int data, BDeque *d);
+
+// Remove Functions
+int removeFromIndex(int i, BDeque *d);
+int removeFromFront(BDeque *d);
+int removeFromBack(BDeque *d);
+
+// Get/Set Functions
+int getIndex(int i, BDeque *d);
+void setIndex(int i, int data, BDeque *d);
+
+// Utility Methods
+void printDeque(BDeque *d);
+void freeDeque(Bdeque *d);
+
