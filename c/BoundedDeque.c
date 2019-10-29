31ab078047a6f674607bbd9f9d5c2e102477565b
stephen <stephen.l.bell@gmail.com>
2019-10-29 17:07:59 -0400

    Create addToIndex function for Bounded Deque

--- c/BoundedDeque.c
+++ c/BoundedDeque.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "BoundedDeque.h"
 
@@ -11,5 +12,37 @@ BDeque * newBoundedDeque(int maxSize) {
     d->dataList = malloc(sizeof(int) * maxSize);
 
     return d;
+}
+
+void addToIndex(int i, int data, BDeque *d) {
+    if (i > d->size+1) {
+	printf("Index %d is out of bounds.\n", i);
+	return;
+    }
+
+    if (i == d->size)
+	d->dataList[(d->front + d->size)%d->maxSize] = data;
+
+    else {
+
+
+	if (i <= d->size / 2) {
+	    d->front = (d->front == 0) ? d->maxSize - 1 : d->front - 1;
+	    int index = (d->front + i) % d->maxSize;
+
+	    for (int j = 0; j < i; i++) {
+		printf("It shouldn't be entering here\n");
+		d->dataList[(d->front+i)%d->maxSize] = d->dataList[(d->front+i+1)%d->maxSize];
+	    }
+
+	    d->dataList[index] = data;
+	} else {
+	    for (int j = d->size; j > i; i--) {
+		d->dataList[(d->front+i)%d->maxSize] = d->dataList[(d->front+i-1)%d->maxSize];
+	    }
+	}	
+    }
+
+    d->size++;
 }
 
