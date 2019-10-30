9fce88d0346eced63d3bbd0d6dd164f043a292bd
stephen <stephen.l.bell@gmail.com>
2019-10-29 20:41:10 -0400

    Update SELList.h to use Bounded Deque

--- c/SELList.h
+++ c/SELList.h
@@ -1,6 +1,8 @@
+#include "BoundedDeque.h"
+
 /* Type Definitions */
 typedef struct SELLNode {
-    int *dataList;  // pointer to a list of int values
+    BDeque *deque;
     struct SELLNode *prev;
     struct SELLNode *next;
 } Node;
@@ -22,18 +24,18 @@ typedef struct Location {
 SELList * newSELList(int blockSize);
 
 // Add methods: adds to front, back, or specified index
-void addToIndex(int i, int data, SELList *list);
+void SELLaddToIndex(int i, int data, SELList *list);
 void addHead(int data, SELList *list);
 void addTail(int data, SELList *list);
 
 // Remove methods: removes from front, back, or specified index
-int removeFromIndex(int i, SELList *list);
+int SELLremoveFromIndex(int i, SELList *list);
 int removeHead(SELList *list);
 int removeTail(SELList *list);
 
 // Get/Set methods
-int getIndex(int i, SELList *list);
-int setIndex(int i, int data, SELList *list);
+int getIndexSELL(int i, SELList *list);
+int setIndexSELL(int i, int data, SELList *list);
 
 // List utility methods
 void printList(SELList *list);
