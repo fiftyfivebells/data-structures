1c6008b5bcca6bf55423f61f5b43366d4397d0f0
stephen <stephen.l.bell@gmail.com>
2019-12-14 16:35:19 -0500

    Add more tests to testing file

--- java_structures/Test.java
+++ java_structures/Test.java
@@ -3,15 +3,17 @@ package java_structures;
 import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Test {
     public static void main(String[] args) {
         List<Integer> ml = new MyArrayList<Integer>(Integer.class);
         List<Integer> al = new ArrayList<Integer>();
+        Random prng = new Random();
 
         for (int i = 0; i < 1000; i++) {
-            ml.add(i);
-            al.add(i);
+            ml.add(i, i);
+            al.add(i, i);
         }
 
         System.out.print("These two lists are equal: ");
@@ -21,13 +23,24 @@ public class Test {
         al.clear();
 
         for (int i = 0; i < 1000; i++) {
-            ml.add(0);
-            al.add(0);
+            ml.add(0, i);
+            al.add(0, i);
         }
 
         System.out.print("These two lists are equal: ");
         System.out.println(ml.equals(al));
 
 
+        ml.clear();
+        al.clear();
+
+        for (int i = 0; i < 10; i++) {
+            int index = prng.nextInt(i+1);
+            ml.add(index, i);
+            al.add(index, i);
+        }
+
+        System.out.print("These two lists are equal: ");
+        System.out.println(ml.equals(al));
     }
 }
