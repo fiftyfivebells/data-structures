23cf7323bf84a26ca78e8b7c1258c2077a88b61e
stephen <stephen.l.bell@gmail.com>
2019-12-14 16:51:31 -0500

    Make ArrayStack class and constructor

---
+++ java_structures/ArrayStack.java
@@ -0,0 +1,15 @@
+package java_structures;
+
+public class ArrayStack extends MyArrayStack<T> {
+
+    /**
+     * Constructor
+     * Calls the parent constructor with the desired class
+     * @param t The class type of the object being held by this stack
+     */
+    public ArrayStack(Class<T> t) {
+        super(t);
+    }
+
+    
+}
